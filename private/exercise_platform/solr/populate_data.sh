92765bbff233b2fd49fccdff5b5d46c2809c9670
Ubuntu <ubuntu@ip-172-30-11-109.ec2.internal>
2017-11-21 22:38:18 +0000

    Add Neo4j, update rest of dbs.

---
+++ private/exercise_platform/solr/populate_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker exec -it --user=solr ceudsd-solr bin/solr create -c dsdcore -n data_driven_schema_configs
+docker cp flights.csv ceudsd-solr:/opt/solr
+docker exec -it --user=solr ceudsd-solr bin/post -c dsdcore flights.cs
+
--- private/exercise_platform/solr/start_solr.sh
+++ private/exercise_platform/solr/start_solr.sh
@@ -2,8 +2,5 @@
 
 BASE_DIR=`pwd`
 
-docker run --name ceudsd-solr -d -p 8083:8983 -v $BASE_DIR/mycores:/opt/solr/server/solr/mycores -t solr
-docker exec -it --user=solr ceudsd-solr bin/solr create -c dsdcore -n data_driven_schema_configs
-docker cp flights.csv ceudsd-solr:/opt/solr
-docker exec -it --user=solr ceudsd-solr bin/post -c dsdcore flights.cs
+docker run --restart unless-stopped --name ceudsd-solr -d -p 8083:8983 -v $BASE_DIR/mycores:/opt/solr/server/solr/mycores -t solr
 
