a7d5eec4cbfb17c760b49f20c9440cdec6c25478
Ubuntu <ubuntu@ip-172-30-11-109.ec2.internal>
2017-11-21 21:29:57 +0000

    Further refine solr config.

--- private/exercise_platform/influxdb/start_influx.sh
+++ private/exercise_platform/influxdb/start_influx.sh
@@ -2,6 +2,9 @@
 
 BASE_DIR=`pwd`
 
-docker network create influxdb
-docker run -d --name=influxdb --net=influxdb -v $BASE_DIR/db:/var/lib/influxdb influxdb
-docker run -d -p 8082:8888 -e "BASE_PATH=/influx" --net=influxdb chronograf --influxdb-url=http://influxdb:8086
+#docker network create influxdb
+#docker run -d --name=influxdb --net=influxdb -v $BASE_DIR/db:/var/lib/influxdb influxdb
+#docker run -d -p 8082:8888 -e "BASE_PATH=/influx" --net=influxdb chronograf --influxdb-url=http://influxdb:8086
+
+docker run -d -p 8082:8083 -p 8086:8086 --name=influxdb --net=influxdb influxdb:1.0.2
+
--- private/exercise_platform/solr/start_solr.sh
+++ private/exercise_platform/solr/start_solr.sh
@@ -2,5 +2,8 @@
 
 BASE_DIR=`pwd`
 
-docker run --name my_solr -p 8083:8983 -v $BASE_DIR/mycores:/opt/solr/server/solr/mycores -t solr
+docker run --name ceudsd-solr -d -p 8083:8983 -v $BASE_DIR/mycores:/opt/solr/server/solr/mycores -t solr
+docker exec -it --user=solr ceudsd-solr bin/solr create -c dsdcore -n data_driven_schema_configs
+docker cp flights.csv ceudsd-solr:/opt/solr
+docker exec -it --user=solr ceudsd-solr bin/post -c dsdcore flights.cs
 
