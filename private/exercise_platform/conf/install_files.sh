8634be68dd0a89c48c3df7000a37559bfa2f4f2e
Ubuntu <ubuntu@ip-172-30-11-109.ec2.internal>
2017-11-21 08:53:40 +0000

    Update main webpage.

--- private/exercise_platform/conf/install_files.sh
+++ private/exercise_platform/conf/install_files.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # replace main nginx file
-rm /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-enabled/default
 cp ./nginx/nginx.conf /etc/nginx/sites-enabled/default
+
+# replace the main webpage
+rm -rf /var/www/http/index.html
+cp ./http/index.html /var/www/http/
 
